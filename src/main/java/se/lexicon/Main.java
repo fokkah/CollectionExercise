f9764a5e0b4896b4e5bbfe90320229ec8ef5d933
fokkah <agameesport@gmail.com>
2025-02-10 13:41:44 +0100

    Everything is going as planned. Has #12 left, doing that at a later time.

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -20,37 +20,99 @@ public class Main {
         //while (itr.hasNext()){
             //System.out.println(itr.next());
         //}
+
+        ArrayList<String> weekDaysMinusThursday = new ArrayList<>();
+        weekDaysMinusThursday.add("Monday");
+        weekDaysMinusThursday.add("Tuesday");
+        weekDaysMinusThursday.add("Wednesday");
+        weekDaysMinusThursday.add("Friday");
+        weekDaysMinusThursday.add("Saturday");
+        weekDaysMinusThursday.add("Sunday");
+        System.out.println("Ex3: \n" + weekDaysMinusThursday);
+
+        weekDaysMinusThursday.add(3, "Thursday");
+        System.out.println("Ex3: \n" + weekDaysMinusThursday + "\n");
+
+
+        ArrayList<String> weekDaysList = new ArrayList<>();
+        weekDaysList.add("Monday");
+        weekDaysList.add("Tuesday");
+        weekDaysList.add("Wednesday");
+        weekDaysList.add("Friday");
+        weekDaysList.add("Saturday");
+        weekDaysList.add("Sunday");
+        System.out.println("Ex4: \n" + weekDaysList);
+        List<String> threeDayList = weekDaysList.subList(0,3);
+        System.out.println("Ex4:\n" + threeDayList + "\n");
+
+
+        HashSet<String> weekDayHashSet = new HashSet<>();
+        weekDayHashSet.add("Monday");
+        weekDayHashSet.add("Tuesday");
+        weekDayHashSet.add("Wednesday");
+        weekDayHashSet.add("Thursday");
+        weekDayHashSet.add("Friday");
+        weekDayHashSet.add("Saturday");
+        weekDayHashSet.add("Sunday");
+        System.out.println("Ex5:\n" + weekDayHashSet);
+
+
+        HashSet<String> weekDayHashSet = new HashSet<>();
+        weekDayHashSet.add("Monday");
+        weekDayHashSet.add("Tuesday");
+        weekDayHashSet.add("Wednesday");
+        weekDayHashSet.add("Thursday");
+        weekDayHashSet.add("Friday");
+        weekDayHashSet.add("Saturday");
+        weekDayHashSet.add("Sunday");
+
+        System.out.println("Ex6:\n" + weekDayHashSet);
+
+        ArrayList<String> weekDayHashSetToArrayList = new ArrayList<>(weekDayHashSet);
+        System.out.println("Ex6 To ArrayList: \n" + weekDayHashSetToArrayList);
+
+
+        HashSet<String> randomNames = new HashSet<>();
+        randomNames.add("Tamas");
+        randomNames.add("Botond");
+        randomNames.add("Elnaz");
+        System.out.println("Ex7 Random names: " + randomNames);
+        ArrayList<String> randoomNamesHashToArray = new ArrayList<>(randomNames);
+        randoomNamesHashToArray.stream().sorted();
+        System.out.println("Ex7 Random names sorted: " + randoomNamesHashToArray);
+
+
+        HashSet<String> randomNamesToSort = new HashSet<>();
+        randomNamesToSort.add("Tamas");
+        randomNamesToSort.add("Botond");
+        randomNamesToSort.add("Elnaz");
+        System.out.println("Ex8 Random names: " + randomNamesToSort);
+
+        TreeSet<String> treeSet = new TreeSet<>(randomNamesToSort);
+        System.out.println("Ex8 Random names sorted with TreeSet: " + treeSet);
+
+
+        HashMap<Integer, String> carIdCarBrand = new HashMap<>();
+        carIdCarBrand.put(1, "VolVo");
+        System.out.println("Ex 9:\n" + carIdCarBrand);
+
+        HashMap<Integer, String> carIdCarBrand = new HashMap<>();
+        carIdCarBrand.put(1, "VolVo");
+        carIdCarBrand.put(2, "Saab");
+        carIdCarBrand.put(3, "Porsche");
+        System.out.println("Ex 10:\n" + carIdCarBrand);
+        for (Map.Entry<Integer, String> key : carIdCarBrand.entrySet())
+            System.out.println("Ex 10:\n" + key.getKey());
+
+
+        HashMap<Integer, String> carIdCarBrand = new HashMap<>();
+        carIdCarBrand.put(1, "VolVo");
+        carIdCarBrand.put(2, "Saab");
+        carIdCarBrand.put(3, "Porsche");
+        for (Map.Entry<Integer, String> value : carIdCarBrand.entrySet())
+            System.out.println("Ex 10 Get only Value:\n" + value.getValue());
+
          */
-
-        HashSet<String> weekdayMinusThursday = new HashSet<String>();
-        weekdayMinusThursday.add("Monday");
-        weekdayMinusThursday.add("Tuesday");
-        weekdayMinusThursday.add("Wednesday");
-        weekdayMinusThursday.add("Thursday");
-        weekdayMinusThursday.add("Friday");
-        weekdayMinusThursday.add("Saturday");
-        weekdayMinusThursday.add("Sunday");
-        System.out.println("\nNot sorted\n" + weekdayMinusThursday + "\n");
-
-        List<String> list = new ArrayList<String>(weekdayMinusThursday);
-        Collections.sort(list);
-        System.out.println("Why doesnt this sort it?\n" + list);
-
-        TreeSet<String> treeSet = new TreeSet<>(weekdayMinusThursday);
-        System.out.println(treeSet);
-
-
-
-
-
-        //Iterator<String> itr = set.iterator();
-        //set.remove("Thursday");
-        //System.out.println(set);
-        //set.add("Thursday");
-        //System.out.println(set);
-
-
-
 
 
 
